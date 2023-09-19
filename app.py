d2a623a8ac53db3fec61e8738ed5c201e75b405b
hemantkadam.737@gmail.com <hemantkadam.737@gmail.com>
2023-09-19 17:20:12 +0530

    Logger and Exception Handling Completed

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
+
 
 app= Flask(__name__)
 
@@ -6,7 +10,13 @@ app= Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Starting machine Larning Project"
+    try:
+        raise Exception ("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "CICD Pipeline is estalished"
 
 
 if __name__=="__main__":
